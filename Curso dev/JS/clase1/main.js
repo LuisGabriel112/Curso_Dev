b7e31392d33f3499aa183f25463e0d956b244bbf
Luis Gabriel Venegas Saucedo <83895876+LuisGabriel112@users.noreply.github.com>
2021-10-25 07:57:32 -0500

    La mitad del curso

---
+++ Curso dev/JS/clase1/main.js
@@ -0,0 +1,59 @@
+/* console.log("Hello World") */
+
+/* Los comentarios son asi */
+
+//Hola perra
+
+
+/* "Variable como caja" caja que contiene 'algo' */
+/* var cualquiera = 112312523453512433;
+ */
+/* en este caso 13 */
+/* console.log(cualquiera); */
+
+/* Tipos de datos */
+
+//decimal
+/* var puntoDecimal = 92.1;
+console.log(puntoDecimal); */
+
+//numero negativo
+/* var numeroNegativo = -21;
+
+console.log(numeroNegativo); */
+
+//strings
+/* var string = 'Hola mundo';
+console.log(string); */
+
+/* var loQueSea = 30;
+console.log(loQueSea);
+
+loQueSea = 23;
+console.log(loQueSea); */
+
+/* sustituir */
+/* var equis = 20;
+
+var equis2 = equis;
+
+var equis3 = equis2;
+
+console.log(equis);
+console.log(equis2);
+console.log(equis3); */
+
+//operadores
+// + - / *
+/* 
+var suma = 5+2;
+
+console.log(suma); */
+
+/* var num1 = 14;
+var num2 = 35;
+
+var sum = num1 + num2;
+
+console.log(sum)
+ */
---
+++ Curso dev/JS/clase1/main2.js
@@ -0,0 +1,24 @@
+/* var equis1 = 1;
+var equis2 = 3; */
+
+/* operadores de comparacion basicos
+== > < */
+
+/* condicionales */
+/* 
+if( equis1 < equis2 ){
+    console.log('bienvenido');
+
+} */
+
+var nombre = prompt('Cual es tu nombre?');
+
+var edad = prompt('Cual es tu edad?');
+
+var mayoria = 18
+
+
+if(edad >= mayoria){
+    var alerta = alert('Hola '+nombre+' ya vimos que eres mayor');
+}
+
