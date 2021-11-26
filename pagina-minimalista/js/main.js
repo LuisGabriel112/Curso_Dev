d0caf3365f42072d294fa38c15b1943e99bbe390
Luis Gabriel Venegas Saucedo <83895876+LuisGabriel112@users.noreply.github.com>
2021-11-26 09:07:14 -0600

    update de mi portfolio

--- pagina-minimalista/js/main.js
+++ pagina-minimalista/js/main.js
@@ -1,7 +1,100 @@
-function ir() {
-    Swal.fire(
-        'hechale un vistazo a lo que hice',
-        '<a href="https://www.youtube.com/watch?v=PnNYO_4Tn1o">Ir</a>',
-        'question'
-      )
+function ir(links) {
+  switch (links) {
+    case 1:
+      Swal.fire({
+        title: 'Hechale un vistazo',
+        html: '<a href= "https://luisgabriel112.github.io/Ejercicio/apple/index.html">ve la pagina</a> <br> <br> <a href= "https://github.com/LuisGabriel112/Ejercicio/tree/main/apple">Repositorio</a>',
+        textColor: 'white',
+        width: 600,
+        padding: '3em',
+        background: '#292C2E  url(https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/90ad8742-8913-4731-872c-5c085455f53c/d52w76r-0aa6a96b-3586-4796-9684-cfd58f5e824d.png/v1/fill/w_900,h_480,strp/texture_paper_by_tutorialesisfe_d52w76r-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NDgwIiwicGF0aCI6IlwvZlwvOTBhZDg3NDItODkxMy00NzMxLTg3MmMtNWMwODU0NTVmNTNjXC9kNTJ3NzZyLTBhYTZhOTZiLTM1ODYtNDc5Ni05Njg0LWNmZDU4ZjVlODI0ZC5wbmciLCJ3aWR0aCI6Ijw9OTAwIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.IqLjLg-ObIn68LuovAzWfao86GAWR8IF2x96Mho__Eo)',
+        backdrop: `
+         rgba(12, 12, 12, 0.73)
+        `
+      })
+      break;
+      case 2:
+        Swal.fire({
+          title: 'Hechale un vistazo',
+          html: '<a href= "https://luisgabriel112.github.io/Ejercicio/Classroom/index.html">ve la pagina</a> <br> <br> <a href= "https://github.com/LuisGabriel112/Ejercicio/tree/main/Classroom">Repositorio</a>',
+          textColor: 'white',
+          width: 600,
+          padding: '3em',
+          background: '#292C2E  url(https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/90ad8742-8913-4731-872c-5c085455f53c/d52w76r-0aa6a96b-3586-4796-9684-cfd58f5e824d.png/v1/fill/w_900,h_480,strp/texture_paper_by_tutorialesisfe_d52w76r-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NDgwIiwicGF0aCI6IlwvZlwvOTBhZDg3NDItODkxMy00NzMxLTg3MmMtNWMwODU0NTVmNTNjXC9kNTJ3NzZyLTBhYTZhOTZiLTM1ODYtNDc5Ni05Njg0LWNmZDU4ZjVlODI0ZC5wbmciLCJ3aWR0aCI6Ijw9OTAwIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.IqLjLg-ObIn68LuovAzWfao86GAWR8IF2x96Mho__Eo)',
+          backdrop: `
+           rgba(12, 12, 12, 0.73)
+          `
+        })
+      break;
+      case 3:
+        Swal.fire({
+          title: 'Hechale un vistazo',
+          html: '<a href= "https://luisgabriel112.github.io/Ejercicio/Ejercicio%20FB/index.html">ve la pagina</a> <br> <br> <a href= "https://github.com/LuisGabriel112/Ejercicio/tree/main/Ejercicio%20FB">Repositorio</a>',
+          textColor: 'white',
+          width: 600,
+          padding: '3em',
+          background: '#292C2E  url(https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/90ad8742-8913-4731-872c-5c085455f53c/d52w76r-0aa6a96b-3586-4796-9684-cfd58f5e824d.png/v1/fill/w_900,h_480,strp/texture_paper_by_tutorialesisfe_d52w76r-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NDgwIiwicGF0aCI6IlwvZlwvOTBhZDg3NDItODkxMy00NzMxLTg3MmMtNWMwODU0NTVmNTNjXC9kNTJ3NzZyLTBhYTZhOTZiLTM1ODYtNDc5Ni05Njg0LWNmZDU4ZjVlODI0ZC5wbmciLCJ3aWR0aCI6Ijw9OTAwIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.IqLjLg-ObIn68LuovAzWfao86GAWR8IF2x96Mho__Eo)',
+          backdrop: `
+           rgba(12, 12, 12, 0.73)
+          `
+        })
+        break;
+      case 4:
+        Swal.fire({
+          title: 'Hechale un vistazo',
+          html: '<a href= "https://luisgabriel112.github.io/Ejercicio/practicaaksle/index.html">ve la pagina</a> <br> <br> <a href= "https://github.com/LuisGabriel112/Ejercicio/tree/main/practicaaksle">Repositorio</a>',
+          textColor: 'white',
+          width: 600,
+          padding: '3em',
+          background: '#292C2E  url(https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/90ad8742-8913-4731-872c-5c085455f53c/d52w76r-0aa6a96b-3586-4796-9684-cfd58f5e824d.png/v1/fill/w_900,h_480,strp/texture_paper_by_tutorialesisfe_d52w76r-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NDgwIiwicGF0aCI6IlwvZlwvOTBhZDg3NDItODkxMy00NzMxLTg3MmMtNWMwODU0NTVmNTNjXC9kNTJ3NzZyLTBhYTZhOTZiLTM1ODYtNDc5Ni05Njg0LWNmZDU4ZjVlODI0ZC5wbmciLCJ3aWR0aCI6Ijw9OTAwIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.IqLjLg-ObIn68LuovAzWfao86GAWR8IF2x96Mho__Eo)',
+          backdrop: `
+           rgba(12, 12, 12, 0.73)
+          `
+        })
+        break;
+      case 5:
+          Swal.fire({
+            title: 'Hechale un vistazo',
+            html: '<a href= "https://luisgabriel112.github.io/Ejercicio/Una%20sola%20pagina/index.html">ve la pagina</a> <br> <br> <a href= "https://github.com/LuisGabriel112/Ejercicio/tree/main/Una%20sola%20pagina">Repositorio</a>',
+            textColor: 'white',
+            width: 600,
+            padding: '3em',
+            background: '#292C2E  url(https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/90ad8742-8913-4731-872c-5c085455f53c/d52w76r-0aa6a96b-3586-4796-9684-cfd58f5e824d.png/v1/fill/w_900,h_480,strp/texture_paper_by_tutorialesisfe_d52w76r-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NDgwIiwicGF0aCI6IlwvZlwvOTBhZDg3NDItODkxMy00NzMxLTg3MmMtNWMwODU0NTVmNTNjXC9kNTJ3NzZyLTBhYTZhOTZiLTM1ODYtNDc5Ni05Njg0LWNmZDU4ZjVlODI0ZC5wbmciLCJ3aWR0aCI6Ijw9OTAwIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.IqLjLg-ObIn68LuovAzWfao86GAWR8IF2x96Mho__Eo)',
+            backdrop: `
+             rgba(12, 12, 12, 0.73)
+            `
+          })
+         break;
+      case 6:
+        Swal.fire({
+          title: 'Hechale un vistazo',
+          html: '<a href= "https://luisgabriel112.github.io/Ejercicio/Wix/index.html">ve la pagina</a> <br> <br> <a href= "https://github.com/LuisGabriel112/Ejercicio/tree/main/Wix">Repositorio</a>',
+          textColor: 'white',
+          width: 600,
+          padding: '3em',
+          background: '#292C2E  url(https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/90ad8742-8913-4731-872c-5c085455f53c/d52w76r-0aa6a96b-3586-4796-9684-cfd58f5e824d.png/v1/fill/w_900,h_480,strp/texture_paper_by_tutorialesisfe_d52w76r-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NDgwIiwicGF0aCI6IlwvZlwvOTBhZDg3NDItODkxMy00NzMxLTg3MmMtNWMwODU0NTVmNTNjXC9kNTJ3NzZyLTBhYTZhOTZiLTM1ODYtNDc5Ni05Njg0LWNmZDU4ZjVlODI0ZC5wbmciLCJ3aWR0aCI6Ijw9OTAwIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.IqLjLg-ObIn68LuovAzWfao86GAWR8IF2x96Mho__Eo)',
+          backdrop: `
+           rgba(12, 12, 12, 0.73)
+          `
+        })
+      break;
+    default:
+      break;
+  }
 }
+
+var app = document.getElementById('letras');
+
+var typewriter = new Typewriter(letras, {
+  loop: true
+});
+
+typewriter.typeString('Quien soy?')
+    .pauseFor(2500)
+    .deleteAll()
+    .typeString('Front-end Dev')
+    .pauseFor(2500)
+    .deleteAll()
+    .typeString('<strong>Luis Venegas</strong>')
+    .pauseFor(2500)
+    .start();
