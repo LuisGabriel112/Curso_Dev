f8678c8d119611da4e39ca45fc184442cab020d8
Luis Gabriel Venegas Saucedo <83895876+LuisGabriel112@users.noreply.github.com>
2022-01-24 11:15:58 -0600

    Add files via upload

--- CRYPTAN/dashboard/js/panel.js
+++ CRYPTAN/dashboard/js/panel.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function () {
+/* document.addEventListener('DOMContentLoaded', function () {
     const chart = Highcharts.chart('container', {
         chart: {
             type: 'column'
@@ -35,4 +35,52 @@ let chart; // globally available
                 data: usdtoeur // predefined JavaScript array
            }]
        });
-    });
+    }); */
+
+let urlApi = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=mxn&order=market_cap_desc&per_page=10&page=1'
+
+fetch(urlApi)
+    .then(response => response.json())
+    .then(data => {
+        
+
+        const mappedResults = 
+            Object.keys(data).map(key => {
+                const value = data[key]
+                //console.log(key, '=>',value);
+            //console.log(key, data[key]);
+            let container = document.getElementById('seccion');
+            let element = document.createElement('div');
+            element.className = 'cards'
+            
+            element.innerHTML = `
+                    <img src="${value.image}">
+                    <div class="letter-cards">
+                    <h2>${value.name}</h2>
+                    <h2>${value.current_price}$</h2>
+                    </div>
+                    `;
+    
+                    container.appendChild(element);  
+            //console.log(key, data[key]);
+        })
+
+        //let data2 = console.log(typeof data, 'llo');
+  /*       let index = 0 */
+        /*             for (index in data.results) {
+                        let container = document.getElementsByTagName('main')[0];
+                        let element = document.createElement('div');
+                        element.innerHTML = `
+                        <div class="cards">
+                        <img src="${data.results[index].image}">
+                        <div class="letter-cards">
+                        <h2>${data.results[index].name}</h2>
+                        <h2>${data.results[index].current_price}</h2>
+                        </div>
+                        </div>
+                        `;
+                        container.appendChild(element);
+                    } */
+    })
+
+console.log("hola");
